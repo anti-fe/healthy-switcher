9914faaff9ff6c39f059ea4b52330feb62a6af0f
anti <raylyangosha@gmail.com>
2022-10-07 16:55:51 +0300

    added slider

---
+++ src/js/swiper.js
@@ -0,0 +1,20 @@
+const slider = document.querySelector('.swiper');
+
+const swiper = new Swiper(slider, {
+    slidesPerView: 1,
+    loop: true,
+    preventClicks: true,
+    setWrapperSize: true,
+    autoHeight: true,
+    speed: 400,
+    spaceBetween: 34,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2
+        },
+    }
+  });
